b99051565aa746f4aa913e358e45f10a430f0f60
kirill111111111 <155085103+kirill111111111@users.noreply.github.com>
2024-02-07 00:28:42 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Элементы массива, начиная с конца:");
+        PrintArrayReverse(array, array.Length - 1);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрылось сразу
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write($"{array[index]} ");
+            PrintArrayReverse(array, index - 1);
+        }
+    }
+}
